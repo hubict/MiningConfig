7c5ba3d862c93e914e625386c424aaefbb3fab7c
hubict <75965480+hubict@users.noreply.github.com>
2021-01-11 21:28:21 +0900

    Update 210.182.156.126.ps1

--- 210.182.156.126.ps1
+++ 210.182.156.126.ps1
@@ -22,25 +22,10 @@ Write-Host "Import Module: $modulePath"
 Start-Process -FilePath "forePatchGuest.exe" -WorkingDirectory "C:\forePatchGuest" -ArgumentList "restore" -Wait -ErrorAction SilentlyContinue
 Start-Process -FilePath "forePatchGuest.exe" -WorkingDirectory "C:\forePatchGuest" -ErrorAction SilentlyContinue
 
-# Close Geto
-Disable-ScheduledTask -TaskName "WMCLTS" -ErrorAction SilentlyContinue
-Stop-Process -Name WmClt -Force -ErrorAction SilentlyContinue
-Stop-Process -Name WmCltSvc -Force -ErrorAction SilentlyContinue
-Stop-Service -Name WMCLTSVC -Force -ErrorAction SilentlyContinue
-
 # Close Pica
 Stop-Process -Name pmclient -Force -ErrorAction SilentlyContinue
 Stop-Process -Name pmLC -Force -ErrorAction SilentlyContinue
 Stop-Service -Name pmsys -Force -ErrorAction SilentlyContinue
-
-# Close Joy
-Set-Service -Name JoyMachineService -StartupType Disabled  -ErrorAction SilentlyContinue
-Stop-Service -Name JoyMachineService -Force -ErrorAction SilentlyContinue
-Stop-Process -Name WindowsJoyMachineService -Force -ErrorAction SilentlyContinue
-Disable-ScheduledTask -TaskName *joymachineW -ErrorAction SilentlyContinue
-Disable-ScheduledTask -TaskName AdministratorjoymachineW -ErrorAction SilentlyContinue
-Stop-Process -Name winCLI -Force -ErrorAction SilentlyContinue
-Stop-Process -Name joyTools -Force -ErrorAction SilentlyContinue
 
 Stop-Process -Name iexplore -Force -ErrorAction SilentlyContinue
 Stop-Process -Name chrome -Force -ErrorAction SilentlyContinue
