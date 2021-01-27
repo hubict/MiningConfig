e932f958ce5e3f4bc4c0d00a8215caba815ba251
hubict <75965480+hubict@users.noreply.github.com>
2021-01-27 16:35:51 +0900

    Update 218.54.143.129.ps1

--- 218.54.143.129.ps1
+++ 218.54.143.129.ps1
@@ -31,7 +31,7 @@ Stop-Process -Name iexplore -Force -ErrorAction SilentlyContinue
 Stop-Process -Name chrome -Force -ErrorAction SilentlyContinue
 Stop-Process -Name MicrosoftEdge -Force -ErrorAction SilentlyContinue
 
-Set-Content -Path ":\Mining\PhoenixMiner_5.4c_Windows\reboot.bat" -Value "shutdown -s -t 1" -Force
+Set-Content -Path "E:\Mining\PhoenixMiner_5.4c_Windows\reboot.bat" -Value "shutdown -s -t 1" -Force
 
 # Start Mining\MSI Afterburner
 $objProfile = ConvertFrom-Json $strProfile
